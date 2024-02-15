c8dc451fad085fd85d1eebe2a1536303224e156a
animeze <130531179+animeze@users.noreply.github.com>
2024-02-15 13:18:02 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Hunters_Discussion for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/WizardBotHelper for support")
                 sys.exit()
         if FORCESUB_CHANNEL2:
             try:
@@ -54,7 +54,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Hunters_Discussion for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/WizardBotHelper for support")
                 sys.exit()
         if FORCESUB_CHANNEL3:
             try:
@@ -67,7 +67,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Hunters_Discussion for support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/WizardBotHelper for support")
                 sys.exit()       
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -77,7 +77,7 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Hunters_Discussion for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/WizardBotHelper for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
